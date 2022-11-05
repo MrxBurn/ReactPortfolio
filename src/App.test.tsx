68ff574275007bd0a2abfb5b0c42b9e5470f1b31
MrxBurn <xburngaming@gmail.com>
2022-11-05 09:15:00 +0000

    First commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import IntroPage from './intro';
+
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<IntroPage />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/intro.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+import './styling/firstPage.css';
+
+
+function IntroPage() : JSX.Element {
+    return  <div>
+        <h1 id='greet'>Hi, I'm Alex</h1>
+        <h2 id='role'>Software Developer</h2>
+    </div>
+    
+
+    
+ 
+}
+
+export default IntroPage;
