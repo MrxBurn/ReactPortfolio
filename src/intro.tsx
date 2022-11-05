fc7003075f60950990d536aa45a5790b2e69bc7e
MrxBurn <xburngaming@gmail.com>
2022-11-05 20:58:04 +0000

    Scroll implemented

--- src/intro.tsx
+++ src/intro.tsx
@@ -1,45 +1,25 @@
-import styled from 'styled-components';
-import './styling/firstPage.css';
+import React, { useRef } from "react";
+import SecondPage from "./secondPage";
+import "./styling/firstPage.css";
+import './styling/secondPage.css'
 
-const Greet = styled.h1`
+function IntroPage(): JSX.Element {
+  const sRef = useRef<HTMLDivElement>(null);
 
-    text-align: center;
-    margin: 0;
-    padding: 10rem 0 0;
-    color: white;
-    font-size: 64px;
-    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  const handleClick = () => {
+    sRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
-
-`
-
-const Role = styled.h2`
-    position: absolute;
-    bottom: 5rem;
-    margin-left: auto;
-    margin-right: auto;
-    left: 0;
-    right: 0;
-    text-align: center;
-    margin: 0;
-    padding: 10rem 0 0;
-    color: white;
-    font-size: 48px;
-    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
-    
-
-`
-
-
-function IntroPage() : JSX.Element {
-    return  <div>
-        <Greet>Hi, I'm Alex</Greet>
-        <Role>Software Developer</Role>
-    </div>
-    
-
-    
- 
+  return (
+    <>
+      <div id="firstPage">
+        <h1 id="greet">Hi, I'm Alex</h1>
+        <h2 id="role">Software Developer</h2>
+        <button onClick={ handleClick} id="arrowDown"></button>
+      </div>
+      <div ref={sRef} id="secondPage"></div>
+    </>
+  );
 }
 
 export default IntroPage;
---
+++ src/secondPage.tsx
@@ -0,0 +1,11 @@
+import { RefObject } from 'react';
+import './styling/secondPage.css'
+
+function SecondPage(pRef: HTMLDivElement) : JSX.Element {
+    return (
+        <h1></h1>
+    )
+}
+
+
+export default SecondPage;
