82cedd51b998e53ba514525e73a4649c7cfb70b0
MrxBurn <xburngaming@gmail.com>
2022-11-05 16:36:48 +0000

    .

--- src/intro.tsx
+++ src/intro.tsx
@@ -1,11 +1,40 @@
 import styled from 'styled-components';
 import './styling/firstPage.css';
 
+const Greet = styled.h1`
+
+    text-align: center;
+    margin: 0;
+    padding: 10rem 0 0;
+    color: white;
+    font-size: 64px;
+    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+
+
+`
+
+const Role = styled.h2`
+    position: absolute;
+    bottom: 5rem;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    text-align: center;
+    margin: 0;
+    padding: 10rem 0 0;
+    color: white;
+    font-size: 48px;
+    font-family:'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+    
+
+`
+
 
 function IntroPage() : JSX.Element {
     return  <div>
-        <h1 id='greet'>Hi, I'm Alex</h1>
-        <h2 id='role'>Software Developer</h2>
+        <Greet>Hi, I'm Alex</Greet>
+        <Role>Software Developer</Role>
     </div>
     
 
